5264d2a6d7345d4076a77843cb7864fa7bf34fe7
Vinay171104 <tvinay.jaswanth@gmail.com>
2026-05-08 17:55:02 +0530

    fix: Force CORS headers via custom middleware

--- backend/main.py
+++ backend/main.py
@@ -24,13 +24,13 @@ origins = [
     "http://localhost:3000",
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.middleware("http")
+async def add_cors_header(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
 
 app.include_router(tracking_router)
 app.include_router(alerts_router)
